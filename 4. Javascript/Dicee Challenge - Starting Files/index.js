836707fd0277810a68c368fe701db396957de4bc
jk asare <asare.joseph33@gmail.com>
2023-07-22 12:21:23 +0100

    add dicee game interaction

---
+++ 4. Javascript/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,39 @@
+
+// document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
+
+var randomNumber1 = Math.random();
+
+randomNumber1 = Math.floor(randomNumber1 * 6);
+
+// console.log(randomNumber1);
+//put list of images in array
+var images = [
+    './images/dice1.png',
+    './images/dice2.png',
+    './images/dice3.png',
+    './images/dice4.png',
+    './images/dice5.png',
+    './images/dice6.png'
+];
+
+var images2 = [
+    './images/dice1.png',
+    './images/dice3.png',
+    './images/dice6.png',
+    './images/dice5.png',
+    './images/dice4.png',
+    './images/dice2.png'
+];
+
+//set random images from the array with the random number variable            
+document.querySelector(".img1").setAttribute("src", images[randomNumber1]);
+
+document.querySelector(".img2").setAttribute("src", images2[randomNumber1]);
+
+if(images[randomNumber1] > images2[randomNumber1]){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (images2[randomNumber1] > images[randomNumber1]){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
