50eaafbc7de140bc42ac2f7704d452bff403c011
jk asare <asare.joseph33@gmail.com>
2023-07-23 02:06:59 +0100

    refactor dicee game

--- 4. Javascript/Dicee Challenge - Starting Files/index.js
+++ 4. Javascript/Dicee Challenge - Starting Files/index.js
@@ -1,9 +1,9 @@
 
 // document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
 
-var randomNumber1 = Math.random();
+var randomNumber1 = Math.floor(Math.random() * 6);
 
-randomNumber1 = Math.floor(randomNumber1 * 6);
+var randomNumber2 = Math.floor(Math.random() * 6);
 
 // console.log(randomNumber1);
 //put list of images in array
@@ -16,24 +16,17 @@ var images = [
     './images/dice6.png'
 ];
 
-var images2 = [
-    './images/dice1.png',
-    './images/dice3.png',
-    './images/dice6.png',
-    './images/dice5.png',
-    './images/dice4.png',
-    './images/dice2.png'
-];
-
 //set random images from the array with the random number variable            
 document.querySelector(".img1").setAttribute("src", images[randomNumber1]);
 
-document.querySelector(".img2").setAttribute("src", images2[randomNumber1]);
+document.querySelector(".img2").setAttribute("src", images[randomNumber2]);
 
-if(images[randomNumber1] > images2[randomNumber1]){
+if(images[randomNumber1] > images[randomNumber2]){
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
-} else if (images2[randomNumber1] > images[randomNumber1]){
+} 
+else if (images[randomNumber2] > images[randomNumber1]){
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
-} else {
+}
+ else {
     document.querySelector("h1").innerHTML = "Draw!";
 }
