ba30583025c064e6e17d6f8f72261b7e95a27e3d
jk asare <asare.joseph33@gmail.com>
2023-08-13 15:52:03 +0100

    complete challenge step 4

--- 5. jQuery/Simon Game Challenge Starting Files/game.js
+++ 5. jQuery/Simon Game Challenge Starting Files/game.js
@@ -1,18 +1,33 @@
 
+var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 function nextSequence(){
-    var randomNumber = Math.floor(Math.random() * 4);
-    
-    var randomChosenColour = buttonColours[randomNumber];
 
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    return randomChosenColour;
+
+   $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+   var selectSound = new Audio('sounds/'+randomChosenColour+'.mp3');
+   selectSound.play();
+
+   
 }
 
-//alert(nextSequence());
-var colorSelected = nextSequence();
-//alert(colorSelected);
-$("."+colorSelected).fadeIn().fadeOut().fadeIn();
+
+$(".btn").on("click",function(){
+
+
+   var userChosenColour = $(this).attr("id");
+    //console.log(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+
+    //console.log(userClickedPattern);
+    
+
+   });
+
