20c6cafabbcec789e3d0a942655971f761717b91
jk asare <asare.joseph33@gmail.com>
2023-08-30 15:53:52 +0100

    Update simon game to stage

--- 5. jQuery/Simon Game Challenge Starting Files/game2.js
+++ 5. jQuery/Simon Game Challenge Starting Files/game2.js
@@ -2,6 +2,8 @@ var buttonColours = ["red","blue","green","yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
 function nextSequence(){
 
     var randomNumber = Math.floor(Math.random() * 4);
@@ -11,4 +13,14 @@ function nextSequence(){
 
     $('#'+randomChosenColour).fadeOut(100).fadeIn(100);
     console.log(gamePattern);
+
+    var buttonSound = new Audio("sounds/"+randomChosenColour+".mp3");
+    buttonSound.play();
 }
+
+//detect when buttons are clicked
+$('.btn').click(function(){
+    var userChosenColour = $(this).attr('id');
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+})
