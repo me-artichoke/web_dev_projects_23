45509bf45ea69da3c1c8ac131843a558f3db9123
jk asare <asare.joseph33@gmail.com>
2023-09-19 13:08:03 +0100

    complete superheroes feature

---
+++ Back-end/2.3 NPM/index.js
@@ -0,0 +1,20 @@
+//using npm
+//node comes with npm bundled
+//to use npm, first initialse it. change to working directory first.
+
+//install packages - search on the npm website to find packages, install it, and use it 
+
+//var generateName = require("sillyname");
+// import generateName from "sillyname";
+// var anySillyName = generateName();
+
+// console.log(`My name is ${anySillyName}.`);
+
+
+//generate random superhero names
+import superheroes from "superheroes";
+
+superheroes.all;
+var randomSuperheroes = superheroes.random();
+
+console.log(`I am a ${randomSuperheroes}!`);
